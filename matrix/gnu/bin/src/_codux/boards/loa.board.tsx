87af90e7ef57453bfe7c40d509dea1e8fe54569c
SunLiveMatrix <152664029+SunLiveMatrix@users.noreply.github.com>
2024-01-21 16:28:40 -0300

    ploa
    Codux is an all-in-one visual development environment. Whether you’re starting a new app, creating components, or editing an existing project, every visual change you make reflects instantly in the code and vice versa. To learn more about Codux, visit our website - [https://www.codux.com/](https://www.codux.com/)

---
+++ matrix/gnu/bin/src/_codux/boards/loa.board.tsx
@@ -0,0 +1,41 @@
+import { createBoard } from '@wixc3/react-board';
+
+export default createBoard({
+    name: 'loa',
+    Board: () => 
+    <div>
+        <h1>National Congress approved the Annual Budget Law Project (LOA) for next year
+        </h1>,
+        <p>The National Congress approved this Friday (22/12), the 2024 Annual Budget 
+        Law Project (LOA). The 2024 budget is the first that reflects the priorities 
+        outlined by the population in the discussion of the 2024-2027 Multi-Year Plan 
+        and the rules of the sustainable tax regime. It therefore consolidates the 
+        commitment of President Luiz Inácio Lula da Silva government to fiscal 
+        responsibility and the resumption of inclusive social policies.
+        </p>,
+        <p>Among other expenses assumed by the government in the 2024 budget, the 
+        social commitment is expressed in the real increase of 3% for the minimum wage, 
+        and expenses of R$169 billion for Bolsa Família, R$180 billion for education 
+        and R$ 231 billion in public health, including the family agent program, 
+        vaccination and popular pharmacy, among others. In total, PLOA 2024 foresees 
+        primary expenses of R$2 trillion.
+        </p>,
+        <p>The fiscal target of the 2024 budget is a fiscal balance between revenues 
+        and expenditure on funding and investment (does not consider expenditure on 
+        interest payments), the so-called zero deficit. In practice, the budget law 
+        points to a small surplus of R$3.5 billion in the accounts, but the target 
+        will be considered met if it remains above or below zero by R$28.8 billion 
+        (band of 0.25% for more or less).
+        </p>,
+        <p>According to the final version of the report presented by deputy Luiz 
+        Carlos Motta, total public investments are projected at R$73 billion.
+        Parliamentarians proposed an increase in resources allocated to 
+        parliamentary amendments, and the total for 2024 was R$53 billion. 
+        Parliamentarians also increased the resources allocated to the electoral 
+        fund, setting this item at R$4.9 billion.
+        </p>,
+        <p>Category</p>,
+        <p>Finance, Taxes and Public Management</p>
+    </div>,
+    isSnippet: true,
+});
